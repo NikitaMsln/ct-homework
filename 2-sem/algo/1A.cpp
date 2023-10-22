becafc828ebf1faf8be786c5ca4c21e72ed0b4a2
nikitamsln <nmaslenniov2004@gmail.com>
2023-10-22 18:24:02 +0300

    Update algo

--- 2-sem/algo/1A.cpp
+++ 2-sem/algo/1A.cpp
@@ -4,39 +4,39 @@ using namespace std;
 
 class SegmentTree {
 private:
-	vector<int> data;
-	vector<int[2]> bounds;
-	int size;
-	int data_size;
+	vector<int32_t> data;
+	vector<int32_t[2]> bounds;
+	int32_t size;
+	int32_t data_size;
 
-	inline static int minimum(int x, int y) { return (x < y)? x : y; }
+	inline static int32_t minimum(int32_t x, int32_t y) { return (x < y)? x : y; }
 public:
-	explicit SegmentTree(const vector<int>& array) {
+	explicit SegmentTree(const vector<int32_t>& array) {
 		data_size = 1;
 		for (; data_size < array.size(); data_size *= 2);
 
-		int half_data_size = data_size;
+		int32_t half_data_size = data_size;
 		data_size *= 2;
 
 		size = array.size();
-		data = vector<int>(data_size);
-		bounds = vector<int[2]>(data_size);
+		data = vector<int32_t>(data_size);
+		bounds = vector<int32_t[2]>(data_size);
 
-		for (int i = data_size - 1; i >= half_data_size; i--) {
+		for (int32_t i = data_size - 1; i >= half_data_size; i--) {
 			if (i >= half_data_size + size) data[i] = INT32_MAX;
 			else data[i] = array[i - half_data_size];
 			bounds[i][0] = i - half_data_size;
             bounds[i][1] = bounds[i][0] + 1;
 		}
 
-		for (int i = half_data_size - 1; i > 0; i--) {
+		for (int32_t i = half_data_size - 1; i > 0; i--) {
 			data[i] = minimum(data[i * 2], data[i * 2 + 1]);
 			bounds[i][0] = bounds[i * 2][0];
 			bounds[i][1] = bounds[i * 2 + 1][1];
 		}
 	}
 
-	void set(int index, int value) {
+	void set(int32_t index, int32_t value) {
         if (index < 0 || index >= size) {
             throw exception();
         }
@@ -48,7 +48,7 @@ public:
 		}
 	}
 
-	int min(int left, int right, int index = 1) const {
+	int32_t min(int32_t left, int32_t right, int32_t index = 1) const {
 		if (bounds[index][1] <= left || bounds[index][0] >= right) {
 			return INT32_MAX;
 		} else if (bounds[index][0] >= left && bounds[index][1] <= right) {
@@ -60,17 +60,17 @@ public:
 };
 
 int main() {
-	int n;
+	int32_t n;
 	cin >> n;
-	vector<int> array(n);
-	for (int i = 0; i < n; i++) {
+	vector<int32_t> array(n);
+	for (int32_t i = 0; i < n; i++) {
 		cin >> array[i];
 	}
 	SegmentTree tree(array);
 
 	string s;
 	while (cin >> s) {
-		int a, b;
+		int32_t a, b;
 		cin >> a >> b;
 		if (s == "min") {
 			cout << tree.min(a - 1, b) << "\n";
