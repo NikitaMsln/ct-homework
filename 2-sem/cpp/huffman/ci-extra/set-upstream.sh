1ce0c8447733208626a9ba553623f5c4e84d8d3b
nikitamsln <nmaslenniov2004@gmail.com>
2023-10-22 18:19:49 +0300

    Removed cpp

--- 2-sem/cpp/huffman/ci-extra/set-upstream.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-UPSTREAM_REPO=CPP-KT/huffman-task
--- 2-sem/cpp/huffman/huffman_lib/include/huffman_lib.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-namespace huffman {
-
-void compress(std::istream&, std::ostream&);
-
-void decompress(std::istream&, std::ostream&);
-} // namespace huffman
