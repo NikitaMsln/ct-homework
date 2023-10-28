1eb7599b8ebd90ed01a71e5fa49dc87aa9fc21ec
nikitamsln <nmaslenniov2004@gmail.com>
2023-10-28 03:35:02 +0300

    Fixed DM

--- 3-sem/DM/lab-1/A.cpp
+++ 3-sem/DM/lab-1/A.cpp
@@ -3,16 +3,13 @@
 using namespace std;
 
 
-
 int main() {
     uint32_t n;
     cin >> n;
     vector<vector<bool>> edges(n, vector<bool>(n, false));
-    list<uint32_t> q;
+    deque<uint32_t> q;
     q.push_back(0);
-    edges[0][0] = true;
     for (uint32_t i = 1; i < n; i++) {
-        edges[i][i] = true;
         q.push_back(i);
         string s;
         cin >> s;
@@ -20,7 +17,7 @@ int main() {
             edges[i][j] = edges[j][i] = s[j] == '1';
         }
     }
-    for (uint32_t i = 0; i < n; i++) {
+    for (uint32_t i = 0; i < n * (n - 1); i++) {
         uint32_t curr = q.front();
         q.pop_front();
         if (!edges[curr][q.front()]) {
@@ -36,7 +33,7 @@ int main() {
         }
         q.push_back(curr);
     }
-    for (auto node : q) {
+    for (auto node: q) {
         cout << node + 1 << " ";
     }
     cout << "\n";
